dc000255fa8acc095f481f9e4f9f54a12f3a6249
Abdullah Al Amin <nirjhor18@gmail.com>
2025-08-12 22:51:02 -0400

    OSC parallel ansys license example given

--- PBS_Submit.sh
+++ PBS_Submit.sh
@@ -1,33 +1,34 @@
 #!/bin/sh
 ## Specifying a job name
-#PBS -N ANSYS_test
-## Reserving one node and 36 cpus
-#PBS -l nodes=1:ppn=36
-## Reserving a specific node "node004" and 36 cpus
-##PBS -l nodes=node004:ppn=36
-#PBS -l walltime=03:00:00
-#PBS -l mem=5gb
-#PBS -M aamin1@udayton.edu
-#PBS -e ./error_$PBS_JOBID.txt
-#PBS -o ./output_$PBS_JOBID.txt
-##PBS -V                     #Pass the current environment variables to the job.
+#SBATCH --job-name=ANSYS_test
+## Reserving one node and 1 cpus
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH -L ansys@osc:1,ansyspar@osc:20
+## Reserving a specific node "node004" and 36 cpus (uncomment if needed)
+##SBATCH --nodelist=node004
+#SBATCH --time=00:05:00
+#SBATCH --account=PNS0496
+#SBATCH --mem=5GB
+#SBATCH --mail-user=aamin1@udayton.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --error=./error_%j.txt
+#SBATCH --output=./output_%j.txt
 
-echo Working directory is $PBS_O_WORKDIR
-echo Job name is $PBS_JOBNAME
-cd $PBS_O_WORKDIR
+echo Working directory is $SLURM_SUBMIT_DIR
+echo Job name is $SLURM_JOB_NAME
+cd $SLURM_SUBMIT_DIR
 echo Running on host `hostname`
 echo Time is `date`
 echo Directory is `pwd`
 echo The following processors are allocated to this job:
-echo `cat $PBS_NODEFILE`
-NP=`wc -l < $PBS_NODEFILE`
+echo $SLURM_JOB_NODELIST
+NP=$SLURM_NTASKS
 
 # run the simulation
-
-
-export OMP_NUM_THREADS=36
+echo "Number of threads using SLURM NTASKS: ${SLURM_NTASKS}"
 
 # Load the module for ansys appropriate installation if necessary
-module load ansys/2020R2
+module load ansys/2024R1
 
-fluent 2ddp -t$OMP_NUM_THREADS -g -i ./Example.jou output.txt
+fluent 2ddp -slurm -t ${SLURM_NTASKS} -g -i ./Example.jou
